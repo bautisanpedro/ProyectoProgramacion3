48145c705dcb3fb1b568b900d0674423485d2fa0
bautisanpedro <90062367+bautisanpedro@users.noreply.github.com>
2022-09-05 17:13:43 -0300

    empezandoi

--- proyecto-prog3/src/App.js
+++ proyecto-prog3/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Route, Switch} from 'react-router-dom'
+
+import Nav from './components/Nav/Nav'
+import Footer from './components/Footer/Footer'
+import Home from './screens/Home/Home'
 
 function App() {
+  const opciones = ['pages','admin','tables', 'charts', 'Paseo de perritos', 'Otra opcion']
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Nav opciones={opciones} nombre={'Walter'}/>
+      <main>
+        <Switch>
+          <Route path='/' exact component={Home} />
+        </Switch>
+      </main>
+      <Footer />
+    </React.Fragment>
   );
 }
 
